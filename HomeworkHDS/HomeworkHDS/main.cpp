5ed2b9bccf0d582f9a9101050228af5ec99d66f6
Dong Seok Heo <zest2700@gmail.com>
2022-11-10 14:23:22 +0900

    허동석 : 연습용코드테스트1

--- HomeworkHDS/HomeworkHDS/main.cpp
+++ HomeworkHDS/HomeworkHDS/main.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
-// ㅇㅇ
+
+
+
+
 // 연습용코드 
+
+// 계산함수 
+float Calculation(float _fX, float _fY, float _fPercent)
+{
+	float fX = _fX;
+	float fY = _fY;
+	float fPercent = _fPercent;
+	float fResult = 0.f;
+	int iInput = 0;
+
+	while (true)
+	{
+		// 예외처리
+		if (fX <= 0.f || fY <= 0.f || fPercent < 0.01f || fPercent > 1.0f)
+			continue;
+			
+		// 계산식 
+		fResult = ((fY * fPercent) / fX) * 100.f;
+
+		fResult = floor(fResult * 1000);
+		fResult = fResult / 1000;
+
+		// 계산 완료 후 출력 
+		std::cout << std::fixed;
+		std::cout.precision(4);
+		std::cout << "결과값은 " << fResult << "% 라구욧!!!!!!";
+		std::cout << std::endl;
+		std::cout << std::endl;
+
+		// 출력 후 분기처리
+		std::cout << "1번 : 목료, 재료 고정 후 퍼센트 값만 변경 하여 다시 계산" << std::endl;
+		std::cout << "2번 : 계산 완료 값 출력, 이전에 출력된 내용 전부 삭제" << std::endl;
+		std::cout << std::endl;
+		std::cout << "다른 키 입력시 데스크탑 폭파예정" << std::endl;
+		std::cout << std::endl;
+		std::cin >> iInput;
+		
+		// 예외처리
+		if (iInput < 1 || iInput > 2)
+		{
+			// 폭파
+			std::cout << " !!!!!!!!!!!!!!!!! 펑 !!!!!!!!!!!! " << std::endl;
+			return 0;
+		}
+			
+
+		if (iInput == 1)
+		{
+			std::cout << "=========================재계산^^========================" << std::endl;
+			std::cout << "% 값 입력, 소수점단위로 입력 ex) 0.01 ~ 1.00" << std::endl;
+			std::cout << "범위 이외의 값 설정시 종료" << std::endl;
+			std::cout << "% 값 입력 :  ";
+			std::cin >> fPercent;
+			std::cout << std::endl;
+
+			if (fPercent < 0.01f || fPercent > 1.00f)
+				return 0;
+
+			Calculation(fX, fY, fPercent);
+		}
+
+		if (iInput == 2)
+		{
+			system("cls");
+			std::cout << "최종결과값 :" << fResult << "% 다. 알겠냐 이자식아????";
+			std::cout << "아무키나 누르면 종료" << std::endl;
+			system("pause");
+			return 0;
+		}	
+
+		return 0;
+	}
+}
+
 
 int main()
 {
@@ -12,43 +89,26 @@ int main()
 
 	float fResult = 0.f;
 
-	while (true)
-	{
-		system("cls");
+	int iInput = 0;
 
-		std::cout << "X 값 입력 :  ";
-		std::cin >> fX;
-		
-		if (fX <= 0.f)
-			continue;
-		
-		std::cout << "Y 값 입력 :  ";
-		std::cin >> fY;
-		std::cout << std::endl;
+	// 제가 아닙니다.
+	std::cout << "★엿같은 버섯의 계산기★" << std::endl;
+	std::cout << std::endl;
 
-		if (fY <= 0.f)
-			continue;
+	std::cout << "목료 입력 :  ";
+	std::cin >> fX;
+	
+	std::cout << "재료 입력 :  ";
+	std::cin >> fY;
+	std::cout << std::endl;
 
-		std::cout << "% 값 입력, 소수점단위로 입력해라. ex) 0.01 ~ 1.00" << std::endl;
-		std::cout << "% 값 입력 :  ";
-		std::cin >> fPercent;
-		std::cout << std::endl;
-		
-		if (fPercent < 0.01f || fPercent > 1.0f)
-			continue;
-		
-		fResult = ((fY * fPercent) / fX) * 100.f;
-
-		std::cout << std::fixed;
-		std::cout.precision(2);
-		std::cout << "결과값 : " << fResult << "%";
-		std::cout << std::endl;
-		std::cout << std::endl;
-
-		std::cout << "확인할거 더 있으면 아무키나 눌러라." << std::endl;
-
-		system("pause");
-	}
+	std::cout << "% 값 입력, 소수점단위로 입력 ex) 0.01 ~ 1.00" << std::endl;
+	std::cout << "% 값 입력 :  ";
+	std::cin >> fPercent;
+	std::cout << std::endl;
+	
+	// 계산함수 
+	fResult = Calculation(fX, fY, fPercent);
 
 	return 0;
 }
