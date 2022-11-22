bc9e68a8b9114a1ee05a23134363609213a62f22
Dong Seok Heo <zest2700@gmail.com>
2022-11-22 13:43:02 +0900

    허동석 : 연습코드 커밋

--- HomeworkHDS/20221118Homework/소스.cpp
+++ HomeworkHDS/20221118Homework/소스.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
 
 int PlayerAtt = 20;
 int PlayerHp = 200;
--- HomeworkHDS/HomeworkHDS/main.cpp
+++ HomeworkHDS/HomeworkHDS/main.cpp
@@ -1,107 +1,71 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-// 연습용코드 
 
+// 랜덤뽑기 연습용코드 
 
-enum class EPick
+void Pick(const std::vector<std::string>& _vNameInfo)
 {
-	RECALCULATION = 1,
-	CHECK = 2,
+	int Pick = (int)(_vNameInfo.size());
+	srand((unsigned int)time(0));
+	int a = rand();
+	int Random = rand() % Pick;
 
-	EXIT = 3
-};
-
-// 계산함수 
-int Calculation(float _fX, float _fY, float _fPercent)
-{ 
-	float fX = _fX;
-	float fY = _fY;
-	float fPercent = _fPercent;
-	float fResult = 0.f;
-	int iInput = 0;
-
-	// 계산식 
-	fResult = ((fY * fPercent) / fX) * 100.f;
-	fResult = floor(fResult * 1000);
-	fResult = fResult / 1000;
-
-	// 계산 완료 후 출력 
-	std::cout << std::fixed;
-	std::cout.precision(4);
-	std::cout << "결과값은 " << fResult << "% 라구욧!!!!!!";
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	// 출력 후 분기처리
-	std::cout << "1번 : 목표, 재료 고정 후 퍼센트 값만 변경 하여 다시 계산" << std::endl;
-	std::cout << "2번 : 계산 완료 값 출력, 이전에 출력된 내용 전부 삭제" << std::endl;
-	std::cout << std::endl;
-	std::cout << "다른 키 입력시 초기화" << std::endl;
-	std::cout << std::endl;
-	std::cin >> iInput;
-
-	// 예외처리
-	if (iInput < 1 || iInput > 2)
-		return 0;
-
-	if (iInput == 1)
-	{
-		std::cout << "=========================재계산^^========================" << std::endl;
-		std::cout << "% 값 입력, 소수점단위로 입력 ex) 0.01 ~ 1.00" << std::endl;
-		std::cout << "범위 이외의 값 설정시 종료" << std::endl;
-		std::cout << "% 값 입력 :  ";
-		std::cin >> fPercent;
-		std::cout << std::endl;
-
-		if (fPercent < 0.01f || fPercent > 1.00f)
-			return 0;
-
-		Calculation(fX, fY, fPercent);
-	}
-
-	if (iInput == 2)
-	{
-		system("cls");
-		std::cout << "최종결과값 :" << fResult << "%" << std::endl;
-		std::cout << "알겠냐 이자시가!!!!!" << std::endl;
-		std::cout << "아무키나 누르면 처음부터" << std::endl;
-		system("pause");
-		return 0;
-	}
-
-	return 0;
+	std::cout << _vNameInfo[Random] << " 님 당첨입니다!!!! " << std::endl;
+	system("pause");
 }
 
 
 int main()
 {
+
+	// 문자열을 저장할 동적배열 
+	std::vector<std::string> vNameInfo;
+
+	vNameInfo.push_back("최지혜");
+	vNameInfo.push_back("허동석");
+	vNameInfo.push_back("해근샘");
+	vNameInfo.push_back("샘근해");
+	vNameInfo.push_back("근해샘");
+	vNameInfo.push_back("김김김");
+	vNameInfo.push_back("최최최");
+	vNameInfo.push_back("허허허");
+	vNameInfo.push_back("지나감");
+	vNameInfo.push_back("공부해");
+	vNameInfo.push_back("열심히");
+	vNameInfo.push_back("안진형");
+	vNameInfo.push_back("강민수");
+	vNameInfo.push_back("이권도");
+	vNameInfo.push_back("이유민");
+	vNameInfo.push_back("서대식");
+	vNameInfo.push_back("이준열");
+
 	while (true)
 	{
 		system("cls");
+		
+		std::cout << "랜덤뽑기를 실행하시겠습니까?" << std::endl;
+		std::cout << "1번 : 실행 " << std::endl;
+		std::cout << "2번 : 종료 " << std::endl;
 
-		float fX = 0.f;
-		float fY = 0.f;
-		float fPercent = 0.f;
-		//int iInput = 0;
+		int Input = 0;
+		std::cin >> Input;
 
-		// 제가 아닙니다.
-		std::cout << "★엿같은 버섯의 계산기★" << std::endl;
-		std::cout << std::endl;
+		if (Input == 1)
+		{
+			Pick(vNameInfo);
+		}
 
-		std::cout << "목표 입력 :  ";
-		std::cin >> fX;
+		if (Input == 2)
+		{
+			return 0;
+		}
 
-		std::cout << "재료 입력 :  ";
-		std::cin >> fY;
-		std::cout << std::endl;
-
-		std::cout << "% 값 입력, 소수점단위로 입력 ex) 0.01 ~ 1.00" << std::endl;
-		std::cout << "% 값 입력 :  ";
-		std::cin >> fPercent;
-		std::cout << std::endl;
-
-		// 계산함수 
-		Calculation(fX, fY, fPercent);
+		else
+		{
+			continue;
+		}
 	}
 
 	return 0;
