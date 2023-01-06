dd251c89f8e1937ee5ee0705d41fc9c1fa9cb644
댕 허 <zest2700@gmail.com>
2023-01-06 21:11:34 +0900

    숙제 커밋!!!

--- HomeworkHDS/20230106Homework/main.cpp
+++ HomeworkHDS/20230106Homework/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <GameEngineBase/GameEngineDebug.h>
 
 class Test
 {
 public:
 	Test()
 	{
-		Teststr = "D:\Project\AR45\WINAPI\APIApp\ContentsResources\Image\Heros.bmp";
+		Teststr = "D:\\Project\\AR45\\WINAPI\\APIApp\\ContentsResources\\Image\\Heros.bmp";
 	}
 	~Test()
 	{
@@ -16,26 +17,34 @@ public:
 public:
 	std::string Teststr;
 
-	void MoveParent(const std::string& _Text)
+	void MoveParent()
 	{
 		int Idx = 0;
 		// 현재 내가 가지고 있는 문자열의 길이를 구한다.
 		int Len = strlen(Teststr.c_str());
 
-		// for 문을 역으로 돌아서 문자열을 찾는다. 
-		for (int i = Len; Len > 0; --i)
+		if (2 == Len)
 		{
-			std::cout << Teststr.c_str() + i - 1 << std::endl;
+			// 현재 문자열의 길이가 2라는 것은 D: <--- 의 경로만 남았다는 의미이다. 
+			// assert 
+			MessageBoxAssert("더이상 상위 디렉터리가 존재하지 않습니다.");
+		}
+
+		// for 문을 역으로 돌아서 문자열을 찾는다. 
+		for (int i = Len - 1; Len > 0; --i)
+		{
+			// 하나씩확인 
+			char Text = Teststr.c_str()[i];
 
 			// 같은 문자열을 찾았다면 원하는 그 문자열의 시작 인덱스를 저장
-			if (!(strcmp(Teststr.c_str() + i - 1, _Text.c_str())))
+			if (Teststr.c_str()[i] == '\\')
 			{
-				// 찾았으면 종료
-				// Idx 번째는 H 라는 의미이고, 그전인덱스에 존재하는 \ 도 지워야하기 때문에 - 1
-				Idx = i - 1;
+				Idx = i;
 				break;
 			}
 		}
+
+		
 
 		// 문자열 배열을 만든다.
 		char ChangeText[256] = {};
@@ -49,9 +58,6 @@ public:
 			CTextPtr[i] = Teststr.c_str()[i];
 		}
 
-		// Idx 번째는 문자열의 끝이기 때문에 0 삽입 
-		ChangeText[Idx] = 0;
-
 		// 내가 가진 문자열에 대입 
 		Teststr = ChangeText;
 	}
@@ -62,9 +68,20 @@ int main()
 {
 	Test strTest;
 
-	strTest.MoveParent("WINAPI");
-
+	strTest.MoveParent();
 	std::cout << strTest.Teststr.c_str() << std::endl;
-	
+	strTest.MoveParent();
+	std::cout << strTest.Teststr.c_str() << std::endl;
+	strTest.MoveParent();
+	std::cout << strTest.Teststr.c_str() << std::endl;
+	strTest.MoveParent();
+	std::cout << strTest.Teststr.c_str() << std::endl;
+	strTest.MoveParent();
+	std::cout << strTest.Teststr.c_str() << std::endl;
+	strTest.MoveParent();
+	std::cout << strTest.Teststr.c_str() << std::endl;
+	strTest.MoveParent();
+	std::cout << strTest.Teststr.c_str() << std::endl;
+	strTest.MoveParent();
 	return 0;
 }
